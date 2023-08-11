cc635f0e9a6ecf27e56b9be6c057b29ef87f6949
joyce <joyce.ann.cruz@gmail.com>
2023-08-11 16:17:33 +0800

    Fix linter errors

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -5,26 +5,26 @@ function Calculator() {
   return (
     <div className="calculator">
       <div className="display">0</div>
-      <button className="key function">AC</button>
-      <button className="key function">+/-</button>
-      <button className="key function">%</button>
-      <button className="key operator">/</button>
-      <button className="key digit">7</button>
-      <button className="key digit">8</button>
-      <button className="key digit">9</button>
-      <button className="key operator">x</button>
-      <button className="key digit">4</button>
-      <button className="key digit">5</button>
-      <button className="key digit">6</button>
-      <button className="key operator">-</button>
-      <button className="key digit">1</button>
-      <button className="key digit">2</button>
-      <button className="key digit">3</button>
-      <button className="key operator">+</button>
-      <button className="key digit zero">0</button>
-      <button className="key digit">.</button>
-      <button className="key operator">=</button>
-  </div>
+      <button type="button" className="key function">AC</button>
+      <button type="button" className="key function">+/-</button>
+      <button type="button" className="key function">%</button>
+      <button type="button" className="key operator">/</button>
+      <button type="button" className="key digit">7</button>
+      <button type="button" className="key digit">8</button>
+      <button type="button" className="key digit">9</button>
+      <button type="button" className="key operator">x</button>
+      <button type="button" className="key digit">4</button>
+      <button type="button" className="key digit">5</button>
+      <button type="button" className="key digit">6</button>
+      <button type="button" className="key operator">-</button>
+      <button type="button" className="key digit">1</button>
+      <button type="button" className="key digit">2</button>
+      <button type="button" className="key digit">3</button>
+      <button type="button" className="key operator">+</button>
+      <button type="button" className="key digit zero">0</button>
+      <button type="button" className="key digit">.</button>
+      <button type="button" className="key operator">=</button>
+    </div>
   );
 }
 
