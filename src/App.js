478fedfe22f57f44dd2cfb3679377e7d421d9aab
joyce <joyce.ann.cruz@gmail.com>
2023-08-17 03:20:46 +0800

    Set up routers for different sections

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './components/Home';
 import Calculator from './components/Calculator';
 import QuoteDisplay from './components/Quote';
+import NotMatch from './components/NotMatch';
 
 function App() {
   return (
-    <div className="App">
-      <Calculator />
-      <QuoteDisplay category="education" />
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="calculator" element={<Calculator />} />
+        <Route path="quote" element={<QuoteDisplay category="education" />} />
+        <Route path="*" element={<NotMatch />} />
+      </Route>
+    </Routes>
   );
 }
 
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -16,28 +16,31 @@ function Calculator() {
   };
 
   return (
-    <div className="calculator">
-      <Display props={calculatorState.next || calculatorState.total || '0'} />
-      <button type="button" className="key function" onClick={() => handleButtonClick('AC')}>AC</button>
-      <button type="button" className="key function" onClick={() => handleButtonClick('+/-')}>+/-</button>
-      <button type="button" className="key function" onClick={() => handleButtonClick('%')}>%</button>
-      <button type="button" className="key operator" onClick={() => handleButtonClick('÷')}>÷</button>
-      <button type="button" className="key digit" onClick={() => handleButtonClick('7')}>7</button>
-      <button type="button" className="key digit" onClick={() => handleButtonClick('8')}>8</button>
-      <button type="button" className="key digit" onClick={() => handleButtonClick('9')}>9</button>
-      <button type="button" className="key operator" onClick={() => handleButtonClick('x')}>x</button>
-      <button type="button" className="key digit" onClick={() => handleButtonClick('4')}>4</button>
-      <button type="button" className="key digit" onClick={() => handleButtonClick('5')}>5</button>
-      <button type="button" className="key digit" onClick={() => handleButtonClick('6')}>6</button>
-      <button type="button" className="key operator" onClick={() => handleButtonClick('-')}>-</button>
-      <button type="button" className="key digit" onClick={() => handleButtonClick('1')}>1</button>
-      <button type="button" className="key digit" onClick={() => handleButtonClick('2')}>2</button>
-      <button type="button" className="key digit" onClick={() => handleButtonClick('3')}>3</button>
-      <button type="button" className="key operator" onClick={() => handleButtonClick('+')}>+</button>
-      <button type="button" className="key digit zero" onClick={() => handleButtonClick('0')}>0</button>
-      <button type="button" className="key digit" onClick={() => handleButtonClick('.')}>.</button>
-      <button type="button" className="key operator" onClick={() => handleButtonClick('=')}>=</button>
-    </div>
+    <>
+      <h2>Let&apos;s do some math!</h2>
+      <div className="calculator">
+        <Display props={calculatorState.next || calculatorState.total || '0'} />
+        <button type="button" className="key function" onClick={() => handleButtonClick('AC')}>AC</button>
+        <button type="button" className="key function" onClick={() => handleButtonClick('+/-')}>+/-</button>
+        <button type="button" className="key function" onClick={() => handleButtonClick('%')}>%</button>
+        <button type="button" className="key operator" onClick={() => handleButtonClick('÷')}>÷</button>
+        <button type="button" className="key digit" onClick={() => handleButtonClick('7')}>7</button>
+        <button type="button" className="key digit" onClick={() => handleButtonClick('8')}>8</button>
+        <button type="button" className="key digit" onClick={() => handleButtonClick('9')}>9</button>
+        <button type="button" className="key operator" onClick={() => handleButtonClick('x')}>x</button>
+        <button type="button" className="key digit" onClick={() => handleButtonClick('4')}>4</button>
+        <button type="button" className="key digit" onClick={() => handleButtonClick('5')}>5</button>
+        <button type="button" className="key digit" onClick={() => handleButtonClick('6')}>6</button>
+        <button type="button" className="key operator" onClick={() => handleButtonClick('-')}>-</button>
+        <button type="button" className="key digit" onClick={() => handleButtonClick('1')}>1</button>
+        <button type="button" className="key digit" onClick={() => handleButtonClick('2')}>2</button>
+        <button type="button" className="key digit" onClick={() => handleButtonClick('3')}>3</button>
+        <button type="button" className="key operator" onClick={() => handleButtonClick('+')}>+</button>
+        <button type="button" className="key digit zero" onClick={() => handleButtonClick('0')}>0</button>
+        <button type="button" className="key digit" onClick={() => handleButtonClick('.')}>.</button>
+        <button type="button" className="key operator" onClick={() => handleButtonClick('=')}>=</button>
+      </div>
+    </>
   );
 }
 
